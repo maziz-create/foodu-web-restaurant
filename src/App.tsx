56c0c2c4c4cd8a9c804e944490af0ee1855b968a
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-02-11 23:41:50 +0300

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return <Navbar />;
 }
 
 export default App;
