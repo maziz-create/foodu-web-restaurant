df4e5c1017ddacc51074e86660f9ad39f5154dcf
Mehmet Aziz Algüllü <maacode@hotmail.com>
2022-02-13 01:25:07 +0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import "./App.css";
+
+import Box from "@mui/material/Box";
+
 import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
+import Download from "./components/Download";
+// import OpeningHours from "./components/OpeningHours";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Box>
+        <Navbar />
+        <Slider />
+      </Box>
+      <Box>
+        <Download />
+        {/* <OpeningHours /> */}
+      </Box>
+    </>
+  );
 }
 
 export default App;
